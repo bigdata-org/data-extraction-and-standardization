de462856dd2a0d2a7703180adddc82c7e47fa41e
sathayNEU <balachandar.sa@northeastern.edu>
2025-01-25 21:04:19 -0500

    feat: extracter - az doc int

--- utils/azure_extract.py
+++ utils/azure_extract.py
@@ -1,10 +1,13 @@
 import os
-import fitz  
+import fitz
 import json
+import pandas as pd  
+import io
 from PIL import Image
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.documentintelligence import DocumentIntelligenceClient
 from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
+from utils.s3 import write_image_to_s3, write_dataframe_to_s3, read_pdf_from_s3
 
 def get_doc_int_client():
     endpoint = os.getenv("AZURE_DOC_INT_ENDPOINT")
@@ -12,34 +15,46 @@ def get_doc_int_client():
     document_intelligence_client = DocumentIntelligenceClient(endpoint=endpoint, credential=AzureKeyCredential(key))
     return document_intelligence_client
 
-def extract_texts(poller_result):
-    return poller_result['content'] if 'content' in poller_result else None
+# def extract_texts(poller_result):
+#     return poller_result['content'] if 'content' in poller_result else None
 
-def extract_figures(poller_result):
-    pass
+def extracter(doc_int_client, s3_client, url):
+    log=[]
+    poller = doc_int_client.begin_analyze_document(
+    "prebuilt-layout",
+    AnalyzeDocumentRequest(url_source=url),
+    ) 
+    data = poller.result()  
+    #Process Figure
+    pdf_raw_data = read_pdf_from_s3(s3_client, url)
+    parent_file = url.split('/uploads/')[1]
+    if 'figures' in data:
+        f_trace = extract_figure(s3_client=s3_client, poller_result=data, parent_file=parent_file, pdf_raw_data = pdf_raw_data)
+        log.extend(f_trace)
+    
+    if 'tables' in data:       
+        t_trace = extract_tables(s3_client=s3_client, poller_result=data, parent_file=parent_file)
+        log.extend(t_trace)
+    return log
 
-def extract_tables(poller_result):
-    pass
+def extract_figure(s3_client, poller_result, parent_file, pdf_raw_data):
+    trace=[]
+    for d in poller_result['figures']:
+        _p = d['id'].split('.')
+        page_num, fig_num = int(_p[0]), int(_p[1])
+        public_url = extract_figure_using_bbox(s3_client=s3_client,
+                            pdf_data=pdf_raw_data,
+                            parent_file=parent_file,
+                            page_num=page_num,
+                            id=fig_num,
+                            polygon=d['boundingRegions'][0]['polygon']
+                            )
+        trace.append(public_url)
+    return trace 
 
-def extracter(doc_int_client, url):
-        poller = doc_int_client.begin_analyze_document(
-        "prebuilt-layout",
-        AnalyzeDocumentRequest(url_source=url),
-        )
-        
-        #Process Text
-
-        #Process Image
-        
-        #Process Figure
-
-
-##############################################
-
-
-def extract_img_from_polygon(pdf_data, page_number, polygon, dpi=72):
+def extract_figure_using_bbox(s3_client, pdf_data, parent_file, page_num, id, polygon, dpi=72):
     doc = fitz.open(stream=pdf_data, filetype="pdf")
-    page = doc[page_number-1] #Zero based index
+    page = doc[page_num-1] #Zero based index
     
     pixmap = page.get_pixmap()
     w, h = pixmap.width, pixmap.height
@@ -60,13 +75,44 @@ def extract_img_from_polygon(pdf_data, page_number, polygon, dpi=72):
     image = Image.frombytes("RGB", [w, h], pixmap.samples)
     
     # Crop the image
-    cropped_image = image.crop((x1, y1, x2, y2))
-    
-    cropped_image.show()
-    
+    cropped_image = image.crop((x1, y1, x2, y2))     # cropped_image.show()
+    image_bytes_io = io.BytesIO()
+    cropped_image.save(image_bytes_io, format="JPEG")   
+    public_url = write_image_to_s3(
+                            channel='azure-ai-document-intelligence', 
+                            s3_client=s3_client, image_bytes=image_bytes_io.getvalue(), 
+                            parent_file=parent_file, 
+                            page_num=page_num, 
+                            id=id
+                        )
     doc.close()
-    return cropped_image
+    return public_url
 
+def extract_tables(s3_client, poller_result, parent_file):
+    trace=[]
+    extract = lambda x: [i['content'] for i in x]
+    t_index=dict()
+    if 'tables' not in poller_result:
+        return -1
+    for table in poller_result['tables']:
+        page_num = table['cells'][0]['boundingRegions'][0]['pageNumber'] 
+        if page_num not in t_index:
+            t_index[page_num]=1
+        _table=[]       
+        for _ in range(0,len(table['cells']), table['columnCount']):
+            _table.append(extract(table['cells'][_ : _+table['columnCount']]))
+        df = pd.DataFrame(_table[1:], columns=_table[0])
+        public_url = write_dataframe_to_s3(
+                                channel='azure-ai-document-intelligence', 
+                                s3_client=s3_client,
+                                df=df,
+                                parent_file=parent_file,
+                                page_num=page_num,
+                                id=t_index[page_num]
+                                )
+        t_index[page_num] = t_index[page_num]+1
+        trace.append(public_url)
+    return trace
 
     
         
--- utils/s3.py
+++ utils/s3.py
@@ -1,5 +1,6 @@
 import os
 import boto3
+import fitz
 from botocore.client import BaseClient
 from uuid import uuid4
 from io import BytesIO, StringIO
@@ -21,8 +22,13 @@ def read_pdf_from_s3(s3_client, url):
     url = url.split('/uploads/')
     if len(url)>1 and url[1].endswith('.pdf'):
         file_name = url[1]
-        response = s3_client.get_object(Bucket=bucket_name, Key=f'uploads/{file_name}')
-        return response["Body"].read()
+        try:
+            response = s3_client.get_object(Bucket=bucket_name, Key=f'uploads/{file_name}')
+            pdf_bytes =  response["Body"].read()
+            return pdf_bytes
+        except Exception as e:
+            print(e)
+            return -1
     else :
         return -1
     
@@ -44,16 +50,36 @@ def upload_pdf_to_s3(s3_client, file_bytes_io: BytesIO):
     except Exception as e:
         raise e
     
-def write_dataframe_to_s3(s3_client, df: pd.DataFrame, parent_file='test', page=1):
+def write_dataframe_to_s3(channel, s3_client, df: pd.DataFrame, parent_file, page_num, id):
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index=False)
     csv_buffer.seek(0)
-    bucket_name = os.getenv("BUCKET_NAME")
-    if bucket_name is None :
+    bucket_name, aws_region = os.getenv("BUCKET_NAME"), os.getenv('AWS_REGION')
+    if bucket_name is None or aws_region is None:
         return -1
-    id = uuid4()
     try:
-        s3_client.put_object(Bucket=bucket_name, Key=f'results/{parent_file}/{page}/tables/'+str(id)+'.csv', Body=csv_buffer.getvalue())
+        s3_client.put_object(Bucket=bucket_name, Key=f'results/{channel}/{parent_file}/{page_num}/tables/{id}.csv', Body=csv_buffer.getvalue())
+        object_url = f"https://{bucket_name}.s3.{aws_region}.amazonaws.com/results/{channel}/{parent_file}/{page_num}/tables/{id}.csv"
+        return object_url
     except Exception as e:
         print(e)
         
+def write_markdown_to_s3(channel, s3_client, md, parent_file='test'):    
+    bucket_name = os.getenv("BUCKET_NAME")
+    if bucket_name is None :
+        return -1
+    try:
+        s3_client.put_object(Bucket=bucket_name, Key=f'results/{channel}/{parent_file}/content.md', Body=md)
+    except Exception as e:
+        print(e)    
+        
+def write_image_to_s3(channel, s3_client, image_bytes, parent_file, page_num, id):    
+    bucket_name, aws_region = os.getenv("BUCKET_NAME"), os.getenv('AWS_REGION')
+    if bucket_name is None or aws_region is None:
+        return -1
+    try:
+        s3_client.put_object(Bucket=bucket_name, Key=f'results/{channel}/{parent_file}/{page_num}/images/{id}.jpeg', Body=image_bytes)
+        object_url = f"https://{bucket_name}.s3.{aws_region}.amazonaws.com/results/{parent_file}/{page_num}/images/{id}.jpeg"
+        return object_url
+    except Exception as e:
+        print(e)  
