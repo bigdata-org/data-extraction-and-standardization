1fe4619b64c479f6c369b3c54eb1bac1250f8314
sathayNEU <balachandar.sa@northeastern.edu>
2025-01-27 13:54:28 -0500

    feat: streamlit setup, fix: fastapi response model (utf-8 decode)

---
+++ frontend/streamlit-app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import requests
+import json
+
+st.title('Welcome to the App!')
+
+
+#Test code for markdown rendering --works 
+# file_name = '649af4f1-a280-43b3-8135-1664e7db178b'
+# api_url = f"http://localhost:8000/results/{file_name}"  # Replace with your FastAPI endpoint
+
+# response = requests.get(api_url)
+# res = response.json()
+# st.markdown(res["markdown"])
+
