e43a2959fb10d19934de842f4f65038175112f6a
Tanmay6461 <pawar.ta@northeastern.edu>
2025-01-28 01:09:02 -0500

    added function and api for conversion using docling

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import os 
-from utils.convert_pdf import process_pdf_to_markdown
+from utils import convert_pdf, docling_conversion
 
 # Create a FastAPI application
 app = FastAPI(docs_url="/docs")
@@ -9,16 +9,22 @@ app = FastAPI(docs_url="/docs")
     
 @app.post("/pdfToMd-text/")
 async def convert_pdf_to_md(file: UploadFile = File(...)):
-
     # Save the uploaded file temporarily
     input_pdf_path = f"temp_{file.filename}"
     with open(input_pdf_path, "wb") as f:
         f.write(await file.read())
     # Process the PDF to Markdown
-    markdown_file_path = process_pdf_to_markdown(input_pdf_path)
-
+    markdown_file_path = convert_pdf.process_pdf_to_markdown(input_pdf_path)
     # Remove the temporary input PDF file
     os.remove(input_pdf_path)
-    
     # Return the Markdown file as a response
-    return FileResponse(markdown_file_path, media_type="text/markdown", filename="data_ex.md")
+    return FileResponse(markdown_file_path, media_type="text/markdown", filename="OpenSourceConversion.md")
+
+@app.post("/pdfToMd_docling/")
+async def pdfTOMd_conversion_docling(file: UploadFile = File()):
+    input_pdf_path = f"temp_{file.filename}"
+    with open(input_pdf_path,"wb") as fp:
+        fp.write(await file.read())
+    md_file_path = docling_conversion.pdf_to_md_docling(input_pdf_path)
+    os.remove(input_pdf_path)
+    return FileResponse(md_file_path, media_type="text/markdown", filename="doclingConversion.md")
---
+++ utils/docling_conversion.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from docling.datamodel.base_models import FigureElement, InputFormat, Table
+from docling.datamodel.pipeline_options import PdfPipelineOptions
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling_core.types.doc import ImageRefMode, PictureItem, TableItem
+
+
+
+def pdf_to_md_docling(pdf_path):
+    input_doc_path = Path(pdf_path)
+    output_dir = Path("docling_conversion")
+
+    IMAGE_RESOLUTION_SCALE = 2.0
+
+    pipeline_options = PdfPipelineOptions()
+    pipeline_options.images_scale = IMAGE_RESOLUTION_SCALE
+    pipeline_options.generate_page_images = True
+    pipeline_options.generate_picture_images = True
+
+    doc_converter = DocumentConverter(
+        format_options={
+            InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
+        }
+    )
+
+    conv_res = doc_converter.convert(input_doc_path)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    doc_filename = conv_res.input.file.stem
+
+    # Save markdown with externally referenced pictures
+    md_filename = output_dir / f"{doc_filename}-with-image-refs.md"
+    conv_res.document.save_as_markdown(md_filename, image_mode=ImageRefMode.REFERENCED)
+
+    return md_filename
