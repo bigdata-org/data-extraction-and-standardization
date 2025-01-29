2a291eadfd3a7c74e2e708217b10a83c0ffdfe07
Tanmay6461 <pawar.ta@northeastern.edu>
2025-01-29 18:04:42 -0500

    added function to store extracted tables to s3

--- utils/convert_pdf.py
+++ utils/convert_pdf.py
@@ -2,12 +2,13 @@ import os
 from pypdf import PdfReader
 import hashlib
 from docling.document_converter import DocumentConverter
-from io import BytesIO
+from io import BytesIO, StringIO
 from utils.aws import s3
 import uuid
 from datetime import datetime
 import pdfplumber
 import pandas as pd 
+
 
 
 def extract_unique_images_and_write_to_s3(s3_client,pdf, bucket_name, s3_prefix='pdf_images_extracted/'):
@@ -45,25 +46,50 @@ def extract_unique_images_and_write_to_s3(s3_client,pdf, bucket_name, s3_prefix=
             "message": str(e)
         }
 
-def extract_tables_from_pdf(pdf):
-    tables_folder = "extracted_tables" 
-    os.makedirs(tables_folder, exist_ok=True)  # Ensure the folder exists
- 
-    with pdfplumber.open(pdf) as pdf :
-        for page_number in range(len(pdf.pages)):
-            table=pdf.pages[page_number].extract_table()
-            if table:
-                df = pd.DataFrame(table[1::], columns=table[0])
-                # print(df)
-                csv_path = os.path.join(tables_folder, f"table_page_{page_number}.csv")
-                df.to_csv(csv_path, index= False)
+
+def extract_tables_from_pdf(pdf_path,s3, bucket_name):
+    # Ensure the output folder exists
+    # os.makedirs(tables_folder, exist_ok=True)
+    # bucket_name = os.getenv('BUCKET_NAME')
+    bucket_name = bucket_name
+    result = []
+    # Open the PDF file
+    with pdfplumber.open(pdf_path) as pdf:
+        try:
+            for page_number, page in enumerate(pdf.pages):
+                # Extract table from the current page
+                table = page.extract_table()
+                if table:
+                    # Convert the table into a DataFrame
+                    df = pd.DataFrame(table[1:], columns=table[0])  # Skip the header row
+                    # Save the DataFrame to a CSV file
+                    if (not df.empty and  # Ensure the table isn't empty
+                        df.apply(lambda x: x.str.strip().replace("", None)).notna().sum().sum() > 2  # Ensure non-empty content
+                    ):
+                        # DF to in-memory csv
+                        csv_buffer = StringIO()
+                        df.to_csv(csv_buffer, index=False)
+                        csv_buffer.seek(0)
+                        s3_key = f"extracted_tables/table_number{page_number + 1}.csv"
+                        s3_client = s3
+
+                        s3_client.put_object(
+                        Bucket=bucket_name,
+                        Key=s3_key,
+                        Body=csv_buffer.getvalue()
+                        )
+                    result.append(f"Files successfully uploaded")
+        except Exception as e :
+            result.append(f"Error occured:{str(e)}")
+
+    return result
 
 
-
-def process_pdf_to_markdown(pdf_path, output_dir="pdf_output"):
+def process_pdf_to_markdown(pdf_path):
    
     bucket_name = os.getenv('BUCKET_NAME')
     s3_client = s3.get_s3client()
+    output_dir="temp_pdf_output"
     os.makedirs(output_dir, exist_ok=True)
 
     # Step 1: Convert PDF to intermediate HTML
@@ -89,13 +115,14 @@ def process_pdf_to_markdown(pdf_path, output_dir="pdf_output"):
         fp.write("</body>\n</html>\n")
 
     extract_unique_images_and_write_to_s3(s3_client,pdf_path, bucket_name,"pdf_images_extracted/")
+    extract_tables_from_pdf(pdf_path, s3_client, bucket_name)
     # Step 2: Convert HTML to Markdown using DocumentConverter
     converter = DocumentConverter()
     result = converter.convert(html_file_path)
     markdown_content = result.document.export_to_markdown()
     markdown_data = BytesIO(markdown_content.encode('utf-8'))
     s3_storage=f"generated_markdown/{datetime.now()}.md"
-
+    
     s3_client.upload_fileobj(
         markdown_data,
         bucket_name,
@@ -108,5 +135,6 @@ def process_pdf_to_markdown(pdf_path, output_dir="pdf_output"):
         ExpiresIn = 3600
     )
     # public_md_url = f"https://{bucket_name}.s3.amazonaws.com/{s3_storage}"
+    os.remove(html_file_path)
     
     return presigned_url
